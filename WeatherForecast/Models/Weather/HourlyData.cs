422062577fb6cb7fc959b0d5cc73ee504a93c527
William Fred <william.fred@canea.se>
2025-04-24 16:44:56 +0200

    did not add any props in hourlyData class earlier

--- WeatherForecast/Models/Weather/HourlyData.cs
+++ WeatherForecast/Models/Weather/HourlyData.cs
@@ -2,5 +2,9 @@
 {
     public class HourlyData
     {
+        public List<string> time { get; set; }
+        public List<double> rain { get; set; }
+        public List<double> temperature_2m { get; set; }
+        public List<int> weather_code { get; set; }
     }
 }
